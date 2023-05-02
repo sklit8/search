d8e6bc62f74acf05d38d8208e0e1987844b48ed2
skilt <2369228162@qq.com>
2023-05-02 17:09:55 +0800

    init

---
+++ src/main/java/com/sklit/search/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.sklit.search.mapper;
+
+import com.sklit.search.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author sklit
+ *  
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/sklit/search/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.sklit.search.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sklit.search.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author sklit
+ *  
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
